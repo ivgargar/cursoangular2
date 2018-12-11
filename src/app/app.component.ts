50f8152fd7a9e37bbb30b3cafc403dbb96ebb121
formacion <formacion@ANSFORM43.indra.es>
2018-12-11 10:37:46 +0100

    servicio de notificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NotificationService, NotificationType } from './common-app/notification.service';
+import { LoggerService } from 'src/indra-core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso';
+  title = 'Hola Mundo';
+
+  constructor(notify: NotificationService, out: LoggerService) {
+    notify.add('Esto es una notificación de error');
+    notify.add('Esto es una notificación de aviso', NotificationType.warn);
+    notify.remove(0);
+    notify.add(null);
+    notify.remove(0);
+    out.error('Esto es un error');
+    out.warn('Esto es un warn');
+    out.info('Esto es un info');
+    out.log('Esto es un log');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndraCoreModule } from 'src/indra-core';
+import { IndraCoreModule, LoggerService } from 'src/indra-core';
 import { ClientesModule } from './clientes/clientes.module';
 import { ProveedoresModule } from './proveedores/proveedores.module';
 import { CommonAppModule } from './common-app/common-app.module';
@@ -17,7 +17,9 @@ import { CommonAppModule } from './common-app/common-app.module';
     IndraCoreModule, ClientesModule, ProveedoresModule, CommonAppModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    LoggerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
